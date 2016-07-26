32d0c56ffe9a350a5bc21e1c08bd38988315a1d0
Md Rezaul Karim <rezaul.km105@gmail.com>
2016-07-26 17:04:49 +0200

    init

--- src/main/resources/static/smartshopping/angular/controller/item.controller.js
+++ src/main/resources/static/smartshopping/angular/controller/item.controller.js
@@ -11,9 +11,21 @@ shSmartShoppingApp.controller('ItemController', ['$http', '$scope', '$rootScope'
         $scope.init = function (itemId) {
             $scope.itemId = itemId;
             
+            $scope.itemImageMeta = [];
+            
            var url = "/rest/item/"+itemId;
             $http.get(url).success(function (data, status, headers) {
                 $scope.item = data;
+                if($scope.item.itemImageMeta.length > 5){
+	                for(imageMeta in $scope.item.itemImageMeta){
+	                	 if(imageMeta < 5){
+	                		 $scope.itemImageMeta.push($scope.item.itemImageMeta[imageMeta]);
+	                	 }
+	                }
+                }
+                else{
+                	$scope.itemImageMeta = $scope.item.itemImageMeta;
+                }
             }).error(function (data, status, headers) {
 
             })
@@ -38,6 +50,50 @@ shSmartShoppingApp.controller('ItemController', ['$http', '$scope', '$rootScope'
 
             })
         };
+        
+        $scope.imageGlow =0;
+        $scope.imageGhigh = 4;
+        
+        $scope.handleItemGalaryNext = function(){
+        	
+        	if($scope.imageGhigh < $scope.item.itemImageMeta.length-1){
+        		$scope.itemImageMeta = [];
+        		$scope.imageFullView = "";
+        		$scope.deActivePre = false;
+        		$scope.imageGlow++;
+        		$scope.imageGhigh++;
+        		for( var i = $scope.imageGlow;i<=$scope.imageGhigh;i++){
+        			$scope.itemImageMeta.push($scope.item.itemImageMeta[i]);
+        		}
+        		if($scope.imageGhigh >= $scope.item.itemImageMeta.length-1){
+        			$scope.deActiveNext = true;
+        		}
+        	}
+        	
+        }
+        
+        $scope.handleItemGalaryPre = function(){
+        	if($scope.imageGlow > 0){
+        		$scope.itemImageMeta = [];
+        		$scope.imageFullView = "";
+        		$scope.deActiveNext = false;
+        		$scope.imageGlow--;
+        		$scope.imageGhigh--;
+        		for( var i = $scope.imageGlow;i<=$scope.imageGhigh;i++){
+        			$scope.itemImageMeta.push($scope.item.itemImageMeta[i]);
+        			setTimeout($scope.handleItemGalaryPre, 2000);
+        		}
+        		
+        		if($scope.imageGlow==0){
+            		$scope.deActivePre = true;
+            	}
+        	}
+        	
+        }
+        
+        $scope.showFullImage = function(imagePath){
+        	$scope.imageFullView = imagePath;
+        }
 
         var controller = {};
         return controller;
